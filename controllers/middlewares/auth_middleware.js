6ba2708207b4c6e45966585efa09046dc17ed88f
unknown <surbhi.sachdev@ottonomy.io>
2023-01-18 16:49:27 +0530

    modified routes product controller

--- controllers/middlewares/auth_middleware.js
+++ controllers/middlewares/auth_middleware.js
@@ -26,7 +26,7 @@ export const authCheck = async (req, res, next) => {
 
 export const authorized = async (req,res,next)=>{
 try {
-  if(req.cookies.jwtoken){
+  if(typeof req.cookies.jwtoken!="undefined" || !req.cookies.jwtoken ){
     console.log("hgh",req.cookies.jwtoken)
     const verifyToken = jwt.verify(req.cookies.jwtoken, process.env.JWT_SECRET_KEY);
     console.log(verifyToken.id)
