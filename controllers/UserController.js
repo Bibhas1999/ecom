f634a391b55438784d482d15ba3927d06081dffc
unknown <surbhi.sachdev@ottonomy.io>
2023-01-24 18:22:20 +0530

    added update profile functions

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -106,9 +106,9 @@ static changePassword = async (req,res)=>{
 }  
   static uploadPic = async (req, res) => {
     try {
-      const {user_id} = req.body 
+      // const {user_id} = req.body 
       let profile_pic = {}
-      user_id = "63c7a388f7f1a6a4864768a0"
+      let user_id = "63c7a388f7f1a6a4864768a0"
       if(!user_id) throw new ValidationError("Unauthenticated")
       let user = await User.findOne({_id:user_id})
       if(!user) throw new HTTPError("User Not Found",404)
